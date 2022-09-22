fa3f8634ea8d93cdcc1f29fce80b6f4c5dbc7b89
abbasinfoicon <uid3@infoiconsoftware.com>
2022-09-22 19:02:17 +0530

    asdf

--- src/view/Services.js
+++ src/view/Services.js
@@ -68,59 +68,59 @@ const Services = () => {
                     <div className="bg_green2"></div>
                   </div>
                   <div className="overflow_circle">
-                    <div class='hidden' id="menu1">
+                    <div className='hidden' id="menu1">
                       <i className="fa fa-trophy"></i>
                       <h3>Markenentwicklung</h3>
                     </div>
-                    <div class='hidden' id="menu2">
+                    <div className='hidden' id="menu2">
                       <i className="fa fa-car"></i>
                       <h3>Produktstrategie & Entwicklung</h3>
                     </div>
-                    <div class='hidden' id="menu3">
+                    <div className='hidden' id="menu3">
                       <i className="fa fa-cubes"></i>
                       <h3>Organisation wandel</h3>
                     </div>
-                    <div class='hidden' id="menu4">
+                    <div className='hidden' id="menu4">
                       <i className="fa fa-truck"></i>
                       <h3>Beschaffungsoptimierung</h3>
                     </div>
-                    <div class='hidden' id="menu5">
+                    <div className='hidden' id="menu5">
                       <i className="fa fa-users"></i>
                       <h3>Vertrieb & After-Sales</h3>
                     </div>
-                    <div class='hidden' id="menu6">
+                    <div className='hidden' id="menu6">
                       <i className="fa fa-cloud"></i>
                       <h3>Konzern-ESG Compliance (Fokus Dekarbonisierung)</h3>
                     </div>
-                    <div class='hidden' id="menu7">
+                    <div className='hidden' id="menu7">
                       <i className="fa fa-meetup"></i>
                       <h3>Additive Fertigung</h3>
                     </div>
-                    <div class='hidden' id="menu8">
+                    <div className='hidden' id="menu8">
                       <i className="fa fa-bullhorn"></i>
                       <h3>Alternative Vebrenner kraftstoffe (Synfuels)</h3>
                     </div>
-                    <div class='hidden' id="menu9">
+                    <div className='hidden' id="menu9">
                       <i className="fa fa-cogs"></i>
                       <h3>Ev-Batterie Recycling</h3>
                     </div>
-                    <div class='hidden' id="menu10">
+                    <div className='hidden' id="menu10">
                       <i className="fa fa-deaf"></i>
                       <h3>Elektrifizierung Antriebsstrang</h3>
                     </div>
-                    <div class='hidden' id="menu11">
+                    <div className='hidden' id="menu11">
                       <i className="fa fa-sitemap"></i>
                       <h3>Fahrzeuge E/E Architektur</h3>
                     </div>
-                    <div class='hidden' id="menu12">
+                    <div className='hidden' id="menu12">
                       <i className="fa fa-gavel"></i>
                       <h3>Mobilitat der zukunft & Digitalisierung</h3>
                     </div>
-                    <div class='hidden' id="menu13">
+                    <div className='hidden' id="menu13">
                       <i className="fa fa-line-chart"></i>
                       <h3>Konzem und Wachstumsstrategie (inkl. M&A)</h3>
                     </div>
-                    <div class='visible' id="menu14">
+                    <div className='visible' id="menu14">
                       <h2>SACs Kernkompeten<br />zgebiete</h2>
                     </div>
                   </div>
@@ -129,98 +129,27 @@ const Services = () => {
                     Wertschöpfungskette</div>
 
                   <nav className="menunav">
-                    <ul class='circle'>
-                      <li class='one slice' data-toggle="modal" data-target="#popSelection">
-                        <label for='one' class='circle over' id="1">
-                          <span>
-                            <div>Konzem und<br />
-                              Wachstums<br />strategie (inkl.<br /> M&A)</div>
-                            <i className="fa fa-line-chart"></i>
-                          </span>
-                        </label>
-                      </li>
-                      <li class='two slice' data-toggle="modal" data-target="#popSelection">
-                        <label for='two' class='circle over' id="2">
-                          <span>
-                            <div>Marken<br />entwicklung</div> <i className="fa fa-trophy"></i>
-                          </span>
-                        </label>
-                      </li>
-                      <li class='three slice' data-toggle="modal" data-target="#popSelection">
-                        <label for='three' class='circle over' id="3"><span>
-                          <div>Produkt<br />strategie &<br /> Entwicklung </div><i
-                            className="fa fa-car"></i>
-                        </span></label>
-                      </li>
-                      <li class='four slice' data-toggle="modal" data-target="#popSelection">
-                        <label for='four' class='circle over' id="4"><span>
-                          <div>Organisations<br /> wandel</div>
-                          <i className="fa fa-cubes"></i>
-                        </span></label>
-                      </li>
-                      <li class='five slice' data-toggle="modal" data-target="#popSelection">
-                        <label for='five' class='circle over' id="5"><span>
-                          <div>Beschaffungs<br />optimierung</div>
-                          <i className="fa fa-truck"></i>
-                        </span></label>
-                      </li>
-                      <li class='six slice' data-toggle="modal" data-target="#popSelection">
-                        <label for='six' class='circle over' id="6"><span>
-                          <div>Vertrieb &<br />
-                            After-Sales</div>
-                          <i className="fa fa-users"></i>
-                        </span></label>
-                      </li>
-                      <li class='seven slice gray_color' data-toggle="modal" data-target="#popSelection">
-                        <label for='seven' class='circle over' id="7"><span>
-                          <div>Konzern-ESG<br /> Compliance<br />
-                            (Fokus Dekar<br />bonisierung)</div>
-                          <i className="fa fa-cloud"></i>
-                        </span></label>
-                      </li>
-                      <li class='eight slice gray_color' data-toggle="modal" data-target="#popSelection">
-                        <label for='eight' class='circle over' id="8"><span>
-                          <div>Additive<br />
-                            Fertigung</div>
-                          <i className="fa fa-meetup"></i>
-                        </span></label>
-                      </li>
-                      <li class='nine slice gray_color' data-toggle="modal" data-target="#popSelection">
-                        <label for='nine' class='circle over' id="9"><span>
-                          <div>Alternative<br />
-                            Vebrenner<br /> kraftstoffe<br /> (Synfuels)</div>
-                          <i className="fa fa-bullhorn"></i>
-                        </span></label>
-                      </li>
-                      <li class='ten slice gray_color' data-toggle="modal" data-target="#popSelection">
-                        <label for='ten' class='circle over' id="10"><span>
-                          <div>Ev-Batterie<br />
-                            Recycling</div>
-                          <i className="fa fa-cogs"></i>
-                        </span></label>
-                      </li>
-                      <li class='eleven slice gray_color' data-toggle="modal" data-target="#popSelection">
-                        <label for='eleven' class='circle over' id="11"><span>
-                          <div>Elektrifi<br />zierung<br />
-                            Antriebs<br />strang</div>
-                          <i className="fa fa-deaf"></i>
-                        </span></label>
-                      </li>
-                      <li class='twelve slice gray_color' data-toggle="modal" data-target="#popSelection">
-                        <label for='twelve' class='circle over' id="12"><span>
-                          <div>Fahrzeuge E/E<br />
-                            Architektur</div>
-                          <i className="fa fa-sitemap"></i>
-                        </span></label>
-                      </li>
-                      <li class='thirteen slice gray_color' data-toggle="modal" data-target="#popSelection">
-                        <label for='thirteen' class='circle over' id="13"><span>
-                          <div>Mobilitat der<br />
-                            zukunft
-                            & <br />Digitalisierung</div>
-                          <i className="fa fa-gavel"></i>
-                        </span></label>
-                      </li>
+                    <ul className='circle'>
+                      {error ? (
+                        <>Oh no, there was an error</>
+                      ) : isLoading ? (
+                        <>Loading...</>
+                      ) : isSuccess ? (
+                        <>
+                          {
+                            data.data.slice(0, 13).map((item, index) => (
+                              <li className={`slice${index + 1} slice`} data-toggle="modal" data-target="#popSelection" key={item.id}>
+                                <label htmlFor={`slice${index + 1}`} className='circle over' id={index + 1}>
+                                  <span>
+                                    <div>{item.title}</div>
+                                    <i className={`fa fa-${item.icon}`}></i>
+                                  </span>
+                                </label>
+                              </li>
+                            ))
+                          }
+                        </>
+                      ) : null}
                     </ul>
                   </nav>
 
@@ -246,42 +175,43 @@ const Services = () => {
         </div>
 
         <div className="container-fluid">
-          <OwlCarousel className="row_custom" id="row_custom" {...options}>
+          {error ? (
+            <>Oh no, there was an error</>
+          ) : isLoading ? (
+            <div className="loading">
+              <img src="assets/img/loading.gif" alt="" className="img-fluid" />
+            </div>
+          ) : isSuccess ? (
 
-            {error ? (
-              <>Oh no, there was an error</>
-            ) : isLoading ? (
-              <>Loading...</>
-            ) : isSuccess ? (
-              <>
-                {
-                  data.data.map((item) => (
-                    <div className="col-custom-1" key={item.id}>
-                      <div className="service_flip">
-                        <div className="service-inner">
-                          <div className="service-front">
-                            <img alt={item.title} className="img-responsive" src={item.img} />
-                            <h2>{item.title}</h2>
-                          </div>
+            <OwlCarousel className="row_custom" id="row_custom" {...options}>
+              {
+                data.data.map((item) => (
+                  <div className="col-custom-1" key={item.id}>
+                    <div className="service_flip">
+                      <div className="service-inner">
+                        <div className="service-front">
+                          <img alt={item.title} className="img-responsive" src={item.img} />
+                          <h2>{item.title}</h2>
+                        </div>
 
-                          <div className="service-back">
-                            <div className="content-sec">
-                              <h3>{item.title}</h3>
-                              <p>{item.desc}</p>
+                        <div className="service-back">
+                          <div className="content-sec">
+                            <h3>{item.title}</h3>
+                            <p>{item.desc}</p>
 
-                              <div className="line"></div>
+                            <div className="line"></div>
 
-                              <p>{item.content}</p>
-                            </div>
+                            <p>{item.content}</p>
                           </div>
                         </div>
                       </div>
                     </div>
-                  ))
-                }
-              </>
-            ) : null}
-          </OwlCarousel>
+                  </div>
+                ))
+              }
+            </OwlCarousel>
+
+          ) : null}
         </div>
       </div>
 
